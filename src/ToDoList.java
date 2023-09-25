47c94c10ca0ca5e86e8db2742f6b75f06c7531d7
john <johnmessoa@gmail.com>
2023-09-25 14:38:02 +0200

    two different approches one is string approach and one is int approach

--- src/ToDoList.java
+++ src/ToDoList.java
@@ -5,17 +5,36 @@ import java.util.Scanner;
 public class ToDoList {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
+       /* int a = scan.nextInt();
         int b = scan.nextInt();
         int c = scan.nextInt();
         System.out.println("here is your stored values you typed in");
-        myArray(a, b, c);
-        findMin(a, b, c);
+        int[] arr = myArray(a, b, c);
+        findMin(arr);
+*/
+        String input = scan.nextLine();
+        int[] arr = readIntegers(input);
+        System.out.println(Arrays.toString(arr));
+        int min = findMin(arr);
+        System.out.println(min);
+
+
+
 
 
     }
+//this is with a string method so that i can generate input simply by typing the numbers and separating them with a ",".
+    static int[] readIntegers(String input) {
+        String[] arrayInput = input.split(",");
+        int[] arr = new int[arrayInput.length];
+        for (int i = 0; i < arrayInput.length; i++) {
+            arr[i] = Integer.parseInt(arrayInput[i].trim());
+        }
+        return arr;
+    }
+    // This array method need three separate inputs instead of the string that only needs one input.
 
-    static int[] myArray(int a, int b, int c) {
+    /* static int[] myArray(int a, int b, int c) {
         int[] Arr = new int[3];
         Arr[0] = a;
         Arr[1] = b;
@@ -23,16 +42,14 @@ public class ToDoList {
         System.out.println(Arrays.toString(Arr));
         return Arr;
     }
-
-    static void findMin(int a, int b, int c) {
-        int[] store = myArray(a, b, c);
-        int min = store[0];
-        for (int i = 0; i < store.length; i++) {
-            if (min > store[i]) {
-                min = store[i];
+*/
+    static int findMin(int[] myArray) {
+        int min = myArray[0];
+        for (int i = 0; i < myArray.length; i++) {
+            if (min > myArray[i]) {
+                min = myArray[i];
             }
         }
-        System.out.println("the minimum element is: " + min);
-
+        return min;
     }
 }
